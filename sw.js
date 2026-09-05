086cba04b827225ac5e5bacf31edf91d52e9061c
grazyguy <62521428+grazyguy@users.noreply.github.com>
2026-09-05 12:51:15 +0200

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "maur-strassendienst-v1";
+const CACHE_NAME = "maur-strassendienst-v2";
 
 const APP_FILES = [
     "./",
     "./index.html"
 ];
 
+// Service Worker installieren
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -13,21 +14,29 @@ self.addEventListener("install", event => {
     );
 });
 
+// Alten Cache löschen
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
+        caches.keys().then(keys => {
+            return Promise.all(
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-            )
-        ).then(() => self.clients.claim())
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
+// Dateien laden
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
-            .then(cached => cached || fetch(event.request))
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request);
+            })
     );
 });
