4c8dc067cfb4074e55d2d87e394850e3ee6ece19
grazyguy <62521428+grazyguy@users.noreply.github.com>
2026-09-05 11:06:27 +0200

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -2,10 +2,7 @@ const CACHE_NAME = "maur-strassendienst-v1";
 
 const APP_FILES = [
     "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json"
+    "./index.html"
 ];
 
 self.addEventListener("install", event => {
@@ -30,8 +27,7 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            return cachedResponse || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(cached => cached || fetch(event.request))
     );
 });
