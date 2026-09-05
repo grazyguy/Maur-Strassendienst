f7525e069405285042701644c8ec42980f9a50e6
grazyguy <62521428+grazyguy@users.noreply.github.com>
2026-09-05 12:50:10 +0200

    app.js aktualisieren

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const APP_FILES = [
     "./index.html"
 ];
 
+// Service Worker installieren
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -13,21 +14,29 @@ self.addEventListener("install", event => {
     );
 });
 
+// Alten Cache löschen
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
+        caches.keys().then(keys => {
+            return Promise.all(
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-            )
-        ).then(() => self.clients.claim())
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
+// Dateien laden
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
-            .then(cached => cached || fetch(event.request))
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request);
+            })
     );
 });
